491091a2a99500179fe792087fb376a57167c580
SailorZero <banana.daiquiri88@gmail.com>
2017-06-12 14:48:09 +0300

    kkk

--- ch_recom.R
+++ ch_recom.R
@@ -10,17 +10,22 @@ user_rest$rate = as.numeric(user_rest$rate)
 
 u = user_rest %>% filter(rate > 0) %>% select(restId, userId, rate)
 
+dp_bs1 = read.csv("/students/dvchuprina/proect_kutezh/location.csv")
 s = select(dp_bs1, redtId, name, score, address, lng,lat,cost,tag)
 names(s) = c("restId", "name", "score", "address", "lng","lat","cost","tag")
 s$restId = as.character(s$restId)
+u$restId = as.character(u$restId)
 
 u = left_join(u, s, by = "restId")
 u1 = select(u, userId, restId, rate)
-# write.csv(u3, "ch_test.csv", row.names = F)
 
-u2 = u1 %>% group_by(userId) %>% dplyr::summarise(count = n()) %>% filter(count >150)
+
+u2 = u1 %>% group_by(userId) %>% dplyr::summarise(count = n()) %>% filter(count >50)
 u3 = left_join(u2, u1, by = "userId")
 u3 = select(u3, -count)
+
+u3 = unique(u3)
+# write.csv(u3, "ch_test.csv", row.names = F)
 
 
 library(tidyr)
@@ -30,7 +35,7 @@ rates = select(rates, -userId)
 
 # chinese dudes
 kit_guys = read.csv("/students/dvchuprina/proect_kutezh/kit_guys.csv")
-names(kit_guys) = c("restId", "bei", "tai","fuj","georg","rus")
+names(kit_guys) = c("restId", "bei", "tai","fuj","georg","mad")
 
 rownames(kit_guys) = kit_guys$restId
 kit_guys = select(kit_guys, -restId)
@@ -42,8 +47,9 @@ kit = plyr::rbind.fill(rates, kit_guys)
 # train = kit[-c(1),]
 # test = kit[c(1),]
 
-train = kit[-c(242,243,244,245,246),]
-test = kit[c(242,243,244,245,246),]
+train = kit[-c(3118,3119,3120,3121,3122),]
+test = kit[c(3118,3119,3120,3121,3122),]
+
 
 
 
@@ -61,9 +67,41 @@ recc_model
 
 # recommending
 
-recc_predicted <- predict(object = recc_model, newdata = te, n = 5)
+recc_predicted <- predict(object = recc_model, newdata = te, n = 10)
 recc_predicted
 str(recc_predicted)
 
-recc_user_1 <- recc_predicted@items
-recc_user_1
+# finding rests' names
+recc_user_1 <- recc_predicted@items[[1]]
+rest_user1 <- recc_predicted@itemLabels[recc_user_1]
+
+recc_user_2 <- recc_predicted@items[[2]]
+rest_user2 <- recc_predicted@itemLabels[recc_user_2]
+
+recc_user_3 <- recc_predicted@items[[3]]
+rest_user3 <- recc_predicted@itemLabels[recc_user_3]
+
+recc_user_4 <- recc_predicted@items[[4]]
+rest_user4 <- recc_predicted@itemLabels[recc_user_4]
+
+recc_user_5 <- recc_predicted@items[[5]]
+rest_user5 <- recc_predicted@itemLabels[recc_user_5]
+
+a = as.data.frame(cbind(rest_user1,rest_user2,rest_user3,rest_user4,rest_user5))
+names(a) = c("bei", "tai","fuj","georg","mad")
+
+aa = as.data.frame(t(as.matrix(a)))
+aa <- data.frame(names = row.names(aa), aa)
+rownames(aa) <- NULL
+a1 = aa
+a1 = gather(a1, names1, restId, 2:11)
+a1 =select(a1, -names1)
+
+s1 = select(s, -score)
+s1$tag = as.character(s1$tag)
+ch_rec_shiny = left_join(a1, s1, by = "restId")
+
+# dudes_rec_full = dudes_rec_full[!is.na(dudes_rec_full$lng),]
+
+
+# write.csv(ch_rec_shiny, "ch_rec_shiny.csv", row.names = F)
--- trip_recom.R
+++ trip_recom.R
@@ -4,27 +4,52 @@ library(readr)
 
 # trip rec
 
-install.packages('rsconnect')
-p_t = read.csv("/students/dvchuprina/proect_kutezh/pekin_text_1.csv")
-name = select(p_t, number, title)
-names(name) = c("query_n", "title")
-name = unique(name)
+# install.packages('rsconnect')
+s_t = read.csv("/students/dvchuprina/proect_kutezh/sh_trip_text.csv")
+sh_tr = read.csv("/students/dvchuprina/proect_kutezh/sh_tr.csv")
+sh_tr = select(sh_tr, link, query_n, address, lng,lat,price_min)
+sh_tr = unique(sh_tr)
+sh = left_join(s_t, sh_tr, by = "query_n")
 
-pekin <- read_csv("~/proect_kutezh/pekin.csv")
-pekin = select(pekin, query_n, lng,lat, address, price_min)
-pek = left_join(name, pekin, by = "query_n")
+sh = select(sh,link, query_n, address, lng, lat, price_min, rate, review_auth_id)
+sh$city = "Shanghai"
+
+p_t = read.csv("/students/dvchuprina/proect_kutezh/p_tr.csv")
+pe = select(p_t,link, query_n, address, lng, lat, price_min, rate, review_auth_id)
+pe$city = "Beijing"
+
+# pekin <- read_csv("~/proect_kutezh/pekin.csv")
+trip = rbind(sh, pe)
+# write.csv(trip, "trip_full.csv", row.names = F)
+trip <- read_csv("~/proect_kutezh/trip_full.csv")
+
+t = trip %>% group_by(review_auth_id) %>% dplyr::summarise(count = n()) %>% filter(count >5)
+t3 = left_join(t, trip, by = "review_auth_id")
+t3 = select(t3, -count)
+
+t3 = unique(t3)
+t4 = t3[!is.na(t3$lng),]
+t4 = t4[!is.na(t4$review_auth_id),]
+# write.csv(t4, "trip_test.csv", row.names = F)
+
 
 
 eu_g = read.csv("/students/dvchuprina/proect_kutezh/eu_guys.csv")
 eu_g = select(eu_g, -Spaniard)
 names(eu_g) = c("restId", "ital", "rus","amer","swed","ger")
 
-h = p_t %>% select(number, review_auth_id, stars)
+# adding num - new restID
+t4 = read.csv("/students/dvchuprina/proect_kutezh/trip_test.csv")
+t4$price_min[is.na(t4$price_min)] = 1000
+h = t4 %>% select(num, review_auth_id, rate)
 h = unique(h)
 
-h1 = h %>% group_by(number, review_auth_id) %>% dplyr::summarise(rate = mean(stars))
-names(h) = c("restId", "revId","stars")
-h3 = select(h, restId)
+h1 = h %>% group_by(num, review_auth_id) %>% dplyr::summarise(rate = mean(rate))
+
+names(h1) = c("restId", "revId","stars")
+h3 = h1 %>%
+  ungroup() %>%
+  select(restId)
 
 
 h2 = inner_join(h3, eu_g, by = "restId")
@@ -33,14 +58,15 @@ h2 = unique(h2)
 
 
 library(tidyr)
-rates_t = spread(h1, key = number, value = rate)
+h11 = h1 %>% ungroup()
+rates_t = spread(h11, key = restId, value = stars)
 
-rownames(rates_t) = rates_t$review_auth_id
-rates_t = select(rates_t, -review_auth_id)
+rownames(rates_t) = rates_t$revId
+rates_t = select(rates_t, -revId)
 
 
 # chinese dudes
-eu_guys = read.csv("/students/dvchuprina/proect_kutezh/eu_guys.csv")
+# eu_guys = read.csv("/students/dvchuprina/proect_kutezh/eu_guys.csv")
 eu_guys = h2
 # names(eu_guys) = c("restId", "ital", "rus","amer","swed","ger", "span")
 
@@ -53,8 +79,8 @@ eu = plyr::rbind.fill(rates_t, eu_guys)
 
 
 
-train_t = eu[-c(6649,6650,6651,6652,6653),]
-test_t = eu[c(6649,6650,6651,6652,6653),]
+train_t = eu[-c(313,314,315,316,317),]
+test_t = eu[c(313,314,315,316,317),]
 
 
 library(recommenderlab)
@@ -75,7 +101,36 @@ recc_predicted_t
 str(recc_predicted_t)
 
 # prediction for italian
+recc_user_1 <- recc_predicted_t@items
+# finding rests' names
 recc_user_1 <- recc_predicted_t@items[[1]]
-a = as.data.frame(recc_user_1)
-names(a) = "query_n"
-it = left_join(a, pek, by = "query_n")
+rest_user1 <- recc_predicted_t@itemLabels[recc_user_1]
+
+recc_user_2 <- recc_predicted_t@items[[2]]
+rest_user2 <- recc_predicted_t@itemLabels[recc_user_2]
+
+recc_user_3 <- recc_predicted_t@items[[3]]
+rest_user3 <- recc_predicted_t@itemLabels[recc_user_3]
+
+recc_user_4 <- recc_predicted_t@items[[4]]
+rest_user4 <- recc_predicted_t@itemLabels[recc_user_4]
+
+recc_user_5 <- recc_predicted_t@items[[5]]
+rest_user5 <- recc_predicted_t@itemLabels[recc_user_5]
+
+a = as.data.frame(cbind(rest_user1,rest_user2,rest_user3,rest_user4,rest_user5))
+names(a) = c("ital", "rus","amer","swed","ger")
+aa = as.data.frame(t(as.matrix(a)))
+aa <- data.frame(names = row.names(aa), aa)
+rownames(aa) <- NULL
+a1 = aa
+a1 = gather(a1, names1, num, 2:11)
+a1 =select(a1, -names1)
+
+t4$num = as.character(t4$num)
+
+dudes_rec_full = left_join(a1, t4, by = "num")
+dudes_rec_full = dudes_rec_full[!is.na(dudes_rec_full$lng),]
+
+
+write.csv(dudes_rec_full, "trip_b_dudes.csv", row.names = F)
